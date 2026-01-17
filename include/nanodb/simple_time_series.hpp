c8be5ecd4165342b647c8d11693484571712684c
Suyash8 <sharmasuyash821+cc@gmail.com>
2026-01-17 10:41:21 +0530

    feat: add bytes method to SimpleTimeSeries and update tests

--- include/nanodb/simple_time_series.hpp
+++ include/nanodb/simple_time_series.hpp
@@ -15,4 +15,8 @@ public:
     size_t size() const {
         return data.size();
     }
+
+    size_t bytes() const {
+        return data.capacity() * sizeof(std::pair<int64_t, double>);
+    }
 };
