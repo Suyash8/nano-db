81c3a66c7f8cb4d0b8a8805053e987b4aa839fcd
Suyash8 <sharmasuyash821+cc@gmail.com>
2026-01-28 00:33:45 +0530

    feat: implement xor value compression with leading/trailing zero optimization

--- include/nanodb/value_compressor.hpp
+++ include/nanodb/value_compressor.hpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 
 #include "bit_writer.hpp"
+#include "utils.hpp"
 
 class ValueCompressor {
     BitWriter& writer_;
@@ -28,8 +29,28 @@ public:
 
         uint64_t xor_val = val_bits ^ prev_bits_;
 
-        if (xor_val == 0) writer_.writeBit(0);
-        else throw std::runtime_error("Non-zero XOR compression not implemented");
+        if (xor_val == 0) {
+            writer_.writeBit(0);
+        } else {
+            int leading = nanodb::countLeadingZeros(xor_val);
+            int trailing = nanodb::countTrailingZeros(xor_val);
+
+            if (leading >= prev_leading_ && trailing >= prev_trailing_) {
+                writer_.writeBits(0b10, 2);
+                int len = 64 - prev_leading_ - prev_trailing_;
+
+                writer_.writeBits(xor_val >> prev_trailing_, len);
+            } else {
+                writer_.writeBits(0b11, 2);
+                writer_.writeBits(leading, 5);
+                int len = 64 - leading - trailing;
+                writer_.writeBits(len, 6);
+
+                writer_.writeBits(xor_val >> trailing, len);
+                prev_leading_ = leading;
+                prev_trailing_ = trailing;
+            }
+        }
 
         prev_bits_ = val_bits;
     }
--- tests/test_value_compression.cpp
+++ tests/test_value_compression.cpp
@@ -15,3 +15,16 @@ TEST(ValueCompressor, CompressesDuplicateValues) {
 
     EXPECT_EQ(writer.getData().size(), 9);  // 8 bytes for first value + 1 bit for each subsequent duplicate
 }
+
+TEST(ValueCompressor, CompressesChangingValues) {
+    BitWriter writer;
+    ValueCompressor val_comp(writer);
+
+    val_comp.addValue(1.0);
+    val_comp.addValue(1.0001);
+    val_comp.addValue(1.0002);
+
+    writer.flush();
+
+    EXPECT_LT(writer.getData().size(), 24);  // Should be less than 3 full doubles (24 bytes)
+}
